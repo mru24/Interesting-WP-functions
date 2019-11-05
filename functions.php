f7e942b9446e706d589ecc3c77ee7ecdb536c715
mru24 <mru24@yahoo.com>
2019-11-05 22:19:20 +0000

    Limit words function added

--- functions.php
+++ functions.php
@@ -21,3 +21,23 @@ remove_all_filters ('wpcf7_before_send_mail');
 add_action( 'wpcf7_before_send_mail', 'contactform7_before_send_mail' );
 
 // ******************************************************************************
+
+// Limit words in text
+function limit_words($words, $limit, $append = ' &hellip;') {
+       // Add 1 to the specified limit becuase arrays start at 0
+       $limit = $limit+1;
+       // Store each individual word as an array element
+       // Up to the limit
+       $words = explode(' ', $words, $limit);
+       // Shorten the array by 1 because that final element will be the sum of all the words after the limit
+       array_pop($words);
+       // Implode the array for output, and append an ellipse
+       $words = implode(' ', $words) . $append;
+       // Return the result
+       return $words;
+}
+
+// Usage - limit words to 15
+$client_excerpt = limit_words($client_excerpt_long, 15, $append = ' <span class="expand-excerpt">[&hellip;]</span>');
+
+// ********************************************************************************
